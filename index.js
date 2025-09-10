024be69a9101ddbf1c1a46d00a678289daa1d8a1
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-09-11 03:38:59 +0530

    DB initialization

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -8,3 +9,7 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.send("Hello From node server");
 });
+
+mongoose.connect(
+  "mongodb+srv://admin_dasun:Dasun@12345@backend.df9r9as.mongodb.net/?retryWrites=true&w=majority&appName=Backend"
+);
