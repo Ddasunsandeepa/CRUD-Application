d849570614468b54740dff304e6008ecb0582181
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-09-13 21:24:14 +0530

    url encoded updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Product = require("./models/productModels.js");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello From node server");
