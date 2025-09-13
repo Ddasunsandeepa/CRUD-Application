0e16244982e2688f59bbff67ba07453fef2f5d8f
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-09-13 22:03:36 +0530

    updated

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const ProductController = require("../controllers/ProductController.js");
-const Product = require("./models/productModels.js");
 
 router.post("/", ProductController.createProducts);
 
@@ -9,17 +8,6 @@ router.get("/", ProductController.getAllProducts);
 
 router.get("/:id", ProductController.getProductById);
 
-router.put("/api/products/:id", ProductController.updateProduct);
+router.put("/:id", ProductController.updateProduct);
 
-app.delete("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    res.status(200).json({ message: "Product deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Server Error" });
-  }
-});
+router.delete("/:id", ProductController.deleteProduct);
