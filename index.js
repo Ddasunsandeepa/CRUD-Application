14a7fd32a42a304066217bf7fdb5d8106e4ff975
Dasun Sandepa <dsk.sandeep.987@gmail.com>
2025-09-13 22:11:21 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const ProductRoute = require("./routes/ProductRoute.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -18,3 +18,4 @@ router.put("/:id", updateProduct);
 
 router.delete("/:id", deleteProduct);
 
+module.exports = router;
